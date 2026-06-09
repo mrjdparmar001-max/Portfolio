33dd9c508654384ecd0d2bc31e89f816bab2fc2b
MRJD 007 <you@example.com>
2026-06-10 01:13:46 +0530

    update

--- user/src/api/api.js
+++ user/src/api/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 API.interceptors.request.use((config) => {
